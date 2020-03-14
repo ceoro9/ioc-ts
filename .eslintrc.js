42c03f248a2d0ac64fc4791ba7a3a71bb1b7f308
Roman Aleksandrovich <ceoro9@protonmail.com>
2020-03-14 18:30:29 +0300

    Set up Prettier with integration to the eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,8 @@ module.exports =  {
   parser:  '@typescript-eslint/parser',
   extends:  [
     'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
   ],
  parserOptions:  {
     ecmaVersion:  2018,
