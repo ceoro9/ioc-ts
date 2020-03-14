545ab129c4d3c0cb963c7458e480ecf595c58a0a
Roman Aleksandrovich <ceoro9@protonmail.com>
2020-03-14 18:26:09 +0300

    Set up ES linter. This closes #5

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports =  {
+  parser:  '@typescript-eslint/parser',
+  extends:  [
+    'plugin:@typescript-eslint/recommended',
+  ],
+ parserOptions:  {
+    ecmaVersion:  2018,
+    sourceType:  'module',
+  },
+  rules:  {
+    "@typescript-eslint/explicit-function-return-type": "off",
+  },
+};
