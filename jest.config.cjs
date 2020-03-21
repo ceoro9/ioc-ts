239235447d59fa32d58eec4ee04dd97ee4fedfa9
Roman Aleksandrovich <ceoro9@protonmail.com>
2020-03-21 18:12:49 +0300

    chore(tests): rename root tests folder

--- jest.config.cjs
+++ jest.config.cjs
@@ -2,4 +2,8 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',  
   setupFiles: ["./jest-setup.ts"],
+  testMatch: [
+    "<rootDir>/tests/**/*.{js,ts}",
+    "<rootDir>/src/**/?(*.)+(spec|test).{js,ts}",
+  ],
 };
